538148f5de4ec928ffc905e380ab9a494e1c23cc
Snyfir <Snyfir@users.noreply.github.com>
2017-11-04 09:36:40 +0100

    add email descriptor

--- script/email.sh
+++ script/email.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # cf http://www.pihomeserver.fr/2015/08/13/envoyer-un-email-depuis-votre-raspberry-pi/
-# exemple: ./email.sh toyou@oxeo.fr subject message
+# run shell script without typing full path: sudo ln -s /home/pi/apps/doxeo-monitor/script/email.sh /usr/local/bin/email
+# exemple: email toyou@oxeo.fr subject message
 echo "$3" | mail -s "$2" $1
 
 exit 0 
