1f4e6f838337130429f8b2dc578877b5823d7dcd
Snyfir <snyfir@users.noreply.github.com>
2018-10-07 13:02:51 +0000

    fix command timeout

--- libraries/scripthelper.cpp
+++ libraries/scripthelper.cpp
@@ -108,7 +108,7 @@ QString ScriptHelper::execute(QString cmd)
 
     process.setWorkingDirectory(QDir::currentPath());
     process.start(cmd);
-    process.waitForFinished(2000);
+    process.waitForFinished(5000);
 
     if (process.exitCode() != 0) {
         qCritical() << "Unable to execute cmd: " << process.readAll() << process.readAllStandardError();
